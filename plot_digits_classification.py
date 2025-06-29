a31b27d06459cf75e6f198640e0f691f3bca7269
Sourin Ghosh <sourin00@gmail.com>
2025-06-29 12:50:09 +0530

    GitHub Actions integration

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,11 +1,11 @@
-
+import pdb
 
 import matplotlib.pyplot as plt
 from utils import run_complete_classification
 
 def main():
 
-
+    pdb.set_trace()
     classifier, y_test, predicted, confusion_matrix_display = run_complete_classification(
         gamma=0.001,
         test_size=0.5,
